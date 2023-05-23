a137150f2a7fa368c8a640aa998f3522a895653b
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-23 14:34:27 +0300

    Done,shoe and Book files passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,26 @@
 
 class Shoe:
     pass
+
+class Shoe:
+    def __init__(self, brand,size):
+        self.brand = brand
+        self._size = size
+
+    
+
+    def get_size(self):
+        return self._size
+        
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
